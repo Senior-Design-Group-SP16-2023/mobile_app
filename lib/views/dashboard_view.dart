2e7b09e5ddc9e2d2a6fbf4bb1c91be9f0ef916ee
talatcanoguz <talatcanoguz0@gmail.com>
2024-02-05 16:38:33 -0600

    created a separate widget for the dashboard header.

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:senior_design/views/widgets/recent_activity.dart';
 import 'package:senior_design/views/widgets/calendar.dart';
-import 'package:intl/intl.dart';
+import 'package:senior_design/views/widgets/dashboard_header.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -14,7 +14,6 @@ class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
     // Format the current date
-    String formattedDate = DateFormat('EEEE, MMMM d').format(DateTime.now());
 
     return Scaffold(
       body: SafeArea( // Use SafeArea to ensure content is not hidden behind status bars or notches
@@ -22,28 +21,7 @@ class _DashboardViewState extends State<DashboardView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0, left: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      formattedDate,
-                      style: TextStyle(
-                        fontSize: 19.0,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    Text(
-                      'Dashboard',
-                      style: TextStyle(
-                        fontSize: 35.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              DashboardHeader(),
               RecentActivity(),
               CalendarWidget(),
             ],
